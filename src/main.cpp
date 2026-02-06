5156540bb68c080ce19c83ccb7f2fbb6e6562a53
dralovets-dot <dralovets@gmail.com>
2026-02-06 11:59:38 +0300

    fix: clean platformio.ini and add ST7789 library

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,13 @@ void setup() {
     Serial.begin(115200);
     delay(1000);
     
-    Serial.println("=== BRUCE RF T-Embed CC1101 ===");
+    Serial.println("=== T-Embed Bruce RF ===");
     
     if(display.begin()) {
-        Serial.println("ST7789 Display: OK");
-        display.drawBruteforceStatus(0xA1B2C3D4, 0, 1250, 0.65f, "RUNNING");
+        Serial.println("Display: OK");
+        display.drawBruteforceStatus(0xA1B2C3D4, 0, 1250, 0.65f, "TEST");
     } else {
-        Serial.println("ST7789 Display: FAILED!");
+        Serial.println("Display: FAIL");
     }
 }
 
